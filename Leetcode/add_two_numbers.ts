e40f2134cbefc364bfc710e81568cb6806b90127
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-13 23:59:05 +0200

    feat: resolve add two numbers leetcode

--- Leetcode/add_two_numbers.ts
+++ Leetcode/add_two_numbers.ts
@@ -11,3 +11,43 @@
 ————————————
     1 7 2
 */
+
+
+  class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.next = (next===undefined ? null : next)
+      }
+  }
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+let carry = 0;
+let prevNode = new ListNode();
+let headNode = prevNode;
+
+while(l1 || l2 || carry) {
+	let val1 = 0;
+	let val2 = 0;
+
+	if (l1) {
+		val1 = l1.val;
+		l1 = l1.next;
+	}
+	if (l2) {
+		val2 = l2.val;
+		l2 = l2.next;
+	}
+
+	const sum = val1 + val2 + carry;
+	carry = Math.floor(sum / 10);
+	const digit = sum % 10;
+
+	const currNode = new ListNode(digit);
+	prevNode.next = currNode;
+	prevNode = currNode;
+}
+
+  return headNode.next;
+};
