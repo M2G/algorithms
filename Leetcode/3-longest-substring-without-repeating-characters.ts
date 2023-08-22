2ecceb7980bb0254e5bd08ff59f0d845c748e588
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-22 02:05:11 +0200

    fix: test casr 1/2 and fzil last

--- Leetcode/3-longest-substring-without-repeating-characters.ts
+++ Leetcode/3-longest-substring-without-repeating-characters.ts
@@ -2,28 +2,23 @@ function lengthOfLongestSubstring(s: string): number {
 
 let maxLength: number = 0;
 
-for (var i = 0; i < s.length; i = i + 1) {
+for (let i = 0; i < s.length; i = i + 1) {
 	// code...
-	for (var j = 0; j < s.length; j = j + 1) {
+	for (let j = i; j < s.length; j = j + 1) {
 		// code...
 		if (checkUniqueSubString(s, i, j)) {
 			maxLength= Math.max(maxLength, j - i + 1)
 		}
 	}
-	return maxLength;
-}
-
-
-
-
-	return 0;
+		return maxLength;
+	}
 };
 
 function checkUniqueSubString(str: string, i: number, j: number) {
 
 const isVisited = {};
 
-	for (var k = i; k <= j; k = k + 1) {
+	for (let k = i; k <= j; k = k + 1) {
 	// code...
 		if (isVisited[str[k]]){
 			return false;
