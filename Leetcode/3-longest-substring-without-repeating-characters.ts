fa1b7ac9c153e1eb088ff7fd6f12a88aff9f0dd0
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-26 03:26:35 +0200

    fix: perfomance

--- Leetcode/3-longest-substring-without-repeating-characters.ts
+++ Leetcode/3-longest-substring-without-repeating-characters.ts
@@ -3,33 +3,22 @@ function lengthOfLongestSubstring(s: string): number {
 let maxLength: number = 0;
 
 for (let i = 0; i < s.length; i += 1) {
+	const isVisited = {};
 	// code...
 	for (let j = i; j < s.length; j += 1) {
 		// code...
-			if (checkUniqueSubString(s, i, j)) {
-				maxLength = Math.max(maxLength, j - i + 1)
+			if (isVisited[s[j]]) {
+				break;
+			} else {
+				maxLength = Math.max(maxLength, j - i + 1);
+				isVisited[s[j]] = true;
 			}
 		}
+		isVisited[s[i]] = false;
 	}
 	return maxLength;
 };
 
-function checkUniqueSubString(str: string, i: number, j: number) {
-
-const isVisited = {};
-
-	for (let k = i; k <= j; k += 1) {
-	// code...
-		if (isVisited[str[k]]){
-			return false;
-		}
-		isVisited[str[k]] = true;
-	}
-
-	console.log('isVisited', isVisited)
-
-	return true;
-}
 
 
 console.log('abcabcbb', lengthOfLongestSubstring("abcabcbb"))
