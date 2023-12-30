08ab01001f49425c6ce16122a76be08b2a46ef71
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-12-30 04:45:26 +0100

    feat: add new algo

--- Leetcode/1-two_sum.ts
+++ Leetcode/1-two_sum.ts
@@ -1,25 +1,10 @@
-/*
-
-Nums = [1,5,9]
-Target = 10
-
-Map = { 1:0 5:1 }
-i = 2
-value = 9
-complement pair = 10 = 9 - 1
-[0,2]
-map[1]
-
-*/
-
-
 function twoSum(nums: number[], target: number): number[] {
       const hash = new Map();
      for (let i = 0; i < nums.length; i += 1) {
         const complement = target - nums[i];
        
         if (hash.has(complement)){
-          return [hash.get(complement), i]
+          return [hash.get(complement), i];
         }
  
         hash.set(nums[i], i);   
