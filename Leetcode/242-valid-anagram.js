388be9310d2ce83dadf3d507067fa425bd6c3d24
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-22 03:43:48 +0100

    feat: add valid anagram (wip)

---
+++ Leetcode/242-valid-anagram.js
@@ -0,0 +1,23 @@
+function isAnagram(s, t) {
+    if (s.length !== t.length)
+        return false;
+    var hashTable = {};
+    for (var i = 0; i < s.length; i += 1) {
+        if (!hashTable[s[i]]) {
+            hashTable[s[i]] = 0;
+            console.log('1', hashTable[s[i]]);
+            console.log('---------');
+        }
+        hashTable[s[i]] += 1;
+        console.log('2', hashTable[s[i]]);
+    }
+    for (var j = 0; j < t.length; j += 1) {
+        if (!hashTable[t[j]]) {
+            return false;
+        }
+        console.log(':::::::::', hashTable[t[j]]);
+    }
+    return false;
+}
+;
+isAnagram("anagram", "nagaram");
---
+++ Leetcode/242-valid-anagram.ts
@@ -0,0 +1,32 @@
+
+
+function isAnagram(s: string, t: string): boolean {
+ if (s.length !== t.length) return false;
+    const hashTable = {};
+
+    for(let i = 0; i < s.length; i += 1) {
+        if (!hashTable[s[i]]) {
+          hashTable[s[i]] = 0;
+           console.log('1',  hashTable[s[i]])
+           console.log('---------')
+        }
+        hashTable[s[i]] += 1;
+            console.log('2',  hashTable[s[i]])
+    }
+
+
+        for(let j = 0; j< t.length; j += 1) {
+        if (!hashTable[t[j]]){
+          return false;  
+        } 
+         console.log(':::::::::', hashTable[t[j]])
+  
+      
+    }
+
+    return false
+
+};
+
+
+isAnagram("anagram", "nagaram");
